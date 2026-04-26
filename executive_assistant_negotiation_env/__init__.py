b1ff7f78d9ee3a2fadfdafb8d6ebf5be820111af
Rakesh <yourgithubemail@gmail.com>
2026-04-26 10:59:35 +0530

    Add root package expected by Streamlit installer

---
+++ executive_assistant_negotiation_env/__init__.py
@@ -0,0 +1,8 @@
+from env import AssistantAction, AssistantObservation, AssistantState, ExecutiveAssistantEnv
+
+__all__ = [
+    "ExecutiveAssistantEnv",
+    "AssistantAction",
+    "AssistantObservation",
+    "AssistantState",
+]
--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name="executive-assistant-negotiation-env",
     version="2.0.0",
-    packages=["env", "server"],
+    packages=["env", "server", "executive_assistant_negotiation_env"],
 )
